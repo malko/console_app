0b4cdda75a170c7973d682cc925510709282db50
malko <malko@196c8fc6-535e-4075-a39a-d285340380c9>
2009-02-05 23:43:22 +0000

    - keep up to date with version of simpleMVC
    git-svn-id: http://svn.jgotti.net/Console_app/trunk@7 196c8fc6-535e-4075-a39a-d285340380c9

--- fx-conf.php
+++ fx-conf.php
@@ -4,7 +4,20 @@
 * @copyleft (l) 2003-2004  Jonathan Gotti
 * @package config
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
-* @changelog - 2007-10-31 - now write_conf_file() keep empty lines.
+* @svnInfos:
+*            - $LastChangedDate$
+*            - $LastChangedRevision$
+*            - $LastChangedBy$
+*            - $HeadURL$
+* @changelog
+*            - 2009-02-05 - parse_conf_file will return empty array on error instead of false when $out is true
+*            - 2009-02-04 - now write_conf_file keep empty lines
+*                         - better support of true|false|null values
+*                         - suppress notice error about $out_ not beeing set when parse_conf_file called on empty files with $out = true
+*                         - bug correction on write_conf_file when working with null values in the config array.
+*            - 2008-10-10 - parse conf_file now ignore already defined CONSTANTS when $out is false
+*            - 2008-02-25 - bug correction when values contain multiple % characters (like urlencoded values)
+*                         - '%%' will be replaced by '%' in values
 *            - 2007-04-30 - bug correction regarding single quote escaped in values string
 *                         - bug correction when saving some multiple multiline values containing '=' at certain position on a line (not the first).
 *            - 2007-04-26 - replace double quote strings with single quote string at eval time to avoid replacement of escaped values (ie: \[rnt....])
@@ -17,45 +30,53 @@
 /**
 * read a config file and define CONSTANT in it or return it as an array
 * @param string $file_path
-* @param bool $out default is FALSE mean define the value, return an array if set to TRUE
+* @param bool $out default is false mean define the value, return an array if set to true
 * @return array | bool depend on $out
 */
 function parse_conf_file($file_path,$out = false){
 	if(! file_exists($file_path))
-		return FALSE;
+		return $out?array():false;
 	# get file content
-  if(! is_array($conf = file($file_path))){
-    return False;
-  }
-  # parse conf file
-	$preserve = FALSE;
+	if(! is_array($conf = file($file_path))){
+		return $out?array():false;
+	}
+
+	if( $out)
+		$out_ = array();
+	$_search = array("/(?<!%)(%(?=[a-z_])([a-z0-9_]+)%)/ie",'!%%!',"!\\\\\s*\r?\n!");
+	$_replce = array("isset(\$out_['\\2'])?\$out_['\\2']:(defined('\\2')?\\2:'\\0');",'%',"\n");
+
+	# parse conf file
+	$preserve = false;
 	foreach($conf as $line){
-    if(preg_match("!^\s*#+!",$line))
-      continue;
-    if($preserve && preg_match("!^\s*(.*?)(\\\\?)\s*$!",$line,$match)){ # continue line
-      $value .= "\n$match[1]";
-      $preserve = ($match[2]!=='\\'?FALSE:TRUE);
-    }elseif(preg_match("!^\s*([^#=]+)=(.*?)(\\\\?)\s*$!",$line,$match)){ # read line
-      $var  = trim($match[1]);
-      $value= $match[2];
-      $preserve = ($match[3]!=='\\'?FALSE:TRUE);
+		if(preg_match("!^\s*#+!",$line))
+			continue;
+		if($preserve && preg_match("!^\s*(.*?)(\\\\?)\s*$!",$line,$match)){ # continue line
+			$value .= "\n$match[1]";
+			$preserve = ($match[2]!=='\\'?false:true);
+		}elseif(preg_match("!^\s*([^#=]+)=(.*?)(\\\\?)\s*$!",$line,$match)){ # read line
+			$var  = trim($match[1]);
+			$value= $match[2];
+			$preserve = ($match[3]!=='\\'?false:true);
 		}else{
-      continue; # considered as commentary
+			continue; # considered as commentary
 		}
-	
-    if($preserve) continue;
-    $value = preg_replace(array("!(%([^%\s]+)%)!","!\\\\\s*\r?\n!"),array("'.\\2.'","\n"),trim($value));
 
-    if(! in_array(strtoupper($value),array('NULL','FALSE','TRUE')) )
-      $value = "'".($out?preg_replace('!(\\\\|\')!','\\\\\1',$value):$value)."'";
+		if($preserve) continue;
+		$value = preg_replace($_search,$_replce,trim($value));
 
-		if(! $out)
-      eval("define('".trim($var)."',".$value.");");
-    else
-      eval('$out_[$var]='.$value.';');
-  }
+		if(! in_array(strtolower($value),array('null','false','true')) )
+			$value = "'".($out?preg_replace('!(\\\\|\')!','\\\\\1',$value):$value)."'";
 
-	return $out?@$out_:TRUE;
+		$var = trim($var);
+		if(! $out){
+			if( ! defined($var) )
+				eval("define('$var',$value);");
+		}else{
+			eval('$out_[$var]='.$value.';');
+		}
+	}
+	return $out?(empty($out_)?array():$out_):true;
 }
 
 /**
@@ -67,59 +88,57 @@ function parse_conf_file($file_path,$out = false){
 * @return bool
 * @changelog 2005-06-11 now can unset or comment some vars using --COMMENT--,--UNSET-- in place of the value
 */
-function write_conf_file($file,$config,$force=FALSE){
-  if(! is_array($config))
-    return FALSE;
+function write_conf_file($file,array $config,$force=false){
+	if(! is_array($config))
+		return false;
 	$fileExist = file_exists($file);
 	# check if file exist or not
 	if( !( $fileExist || $force ) )
-		return FALSE;
+		return false;
 	$oldconf = $fileExist?file($file):null;
-  # get the old config
-  if( is_null($oldconf) && ! $force )
-    return FALSE;
-  # first rewrite old conf
-  if(is_array($oldconf)){
-    $follow = FALSE;
-    foreach($oldconf as $linenb => $line){
-      if( preg_match("!^\s*#!",$line)){# keep comment lines
-        $newconf[$linenb]=$line;
-      }elseif( (!$follow) && preg_match("!^\s*([^#=]+)=([^#\\\\]+)(\\\\?)!",$line,$match)){ # first line of config var
-        $var = trim($match[1]); # get varname
+	# get the old config
+	if( is_null($oldconf) && ! $force )
+		return false;
+	# first rewrite old conf
+	if(is_array($oldconf)){
+		$follow = false;
+		foreach($oldconf as $linenb => $line){
+			if( preg_match("!^\s*(#|$)!",$line)){# keep comment and empty lines
+				$newconf[$linenb]=$line;
+			}elseif( (!$follow) && preg_match("!^\s*([^#=]+)=([^#\\\\]*)(\\\\?)!",$line,$match)){ # first line of config var
+				$var = trim($match[1]); # get varname
 
-        if(! isset($config[$var])) # not set so keep the line as is
-          $newconf[$linenb] = $line;
-        else # we have a new value we write it
-          $newconf[$linenb] = _write_conf_line($var,$config[$var],$line);
+				if(! array_key_exists($var,$config)) # not set so keep the line as is
+					$newconf[$linenb] = $line;
+				else # we have a new value we write it
+					$newconf[$linenb] = _write_conf_line($var,$config[$var],$line);
 
-        if(preg_match('!\\\\\s*\n$!',$line))
-          $follow = TRUE;
-      }elseif($follow){ # multiline values
-        if(!isset($config[$var])){ # keep old multiline values
-          $newconf[$linenb] = $line;
-        }elseif(trim($config[$var])==='--COMMENT--' ){ # comment all multilines values
-          $newconf[$linenb] = "#~ $line";
-        }
-        if(! preg_match('!\\\\\s*\n$!',$line))
-          $follow = FALSE;
-      }else{
-        $newconf[$linenb] = $line;
-      }
-      if( (! $follow) && @isset($config[$var]) )
-        unset($config[$var]);
+				if(preg_match('!\\\\\s*\n$!',$line))
+					$follow = true;
+			}elseif($follow){ # multiline values
+				if(!array_key_exists($var,$config)){ # keep old multiline values
+					$newconf[$linenb] = $line;
+				}elseif(trim($config[$var])==='--COMMENT--' ){ # comment all multilines values
+					$newconf[$linenb] = "#~ $line";
+				}
+				if(! preg_match('!\\\\\s*\n$!',$line))
+					$follow = false;
+			}
+			if( (! $follow) && array_key_exists($var,$config) )
+				unset($config[$var]);
 
-    }
-    if(count($config)>0){ # write new config vars at the end
-      foreach($config as $var=>$value)
-        $newconf[] = _write_conf_line($var,$value);
-    }
-  }elseif($force){
-    foreach($config as $var=>$value)
-      $newconf[] = _write_conf_line($var,$value);
+		}
+		if(count($config)>0){ # write new config vars at the end
+			foreach($config as $var=>$value)
+				$newconf[] = _write_conf_line($var,$value);
+		}
+	}elseif($force){
+		foreach($config as $var=>$value)
+			$newconf[] = _write_conf_line($var,$value);
 	}
-  return array2file(@$newconf,$file);
+	return array2file(@$newconf,$file);
 }
-	
+
 /**
 * take an array and write each value as a line
 * @param array $arr array containings data to write
@@ -127,25 +146,29 @@ function write_conf_file($file,$config,$force=FALSE){
 * @return bool
 */
 function array2file($arr,$file){
-  if(! is_array($arr) ) return FALSE;
-  if(! $f = fopen($file,'w'))
-		return FALSE;
+	if(! is_array($arr) ) return false;
+	if(! $f = fopen($file,'w'))
+		return false;
 	fputs($f,implode('',$arr));
-  return fclose($f);
+	return fclose($f);
 }
 
 /**
 * used by write_conf_file to prepare line for passed values
 */
 function _write_conf_line($var,$value=null,$oldline=null){
-  $commented = (substr_count($value,'--COMMENT--')?TRUE:FALSE);
-  $value = $value?preg_replace("!([^\\\\])\r?\n!",($commented?"\\1\\\\\n# ":"\\1\\\\\n"),$value):$value;
-  if($commented)
-    $line = (($oldline && trim($value)==='--COMMENT--')?"#~ $oldline":"# $var = ".str_replace('--COMMENT--','',$value)."\n");
-  elseif($value === '--UNSET--')
-    $line = '';
-  else
-    $line = "$var = ".$value."\n";
-  return $line;
+	$commented = (substr_count($value,'--COMMENT--')?true:false);
+	if( is_bool($value) )
+		$value = $value?'true':'false';
+	elseif( $value===null)
+		$value = 'null';
+	else
+		$value = preg_replace("!([^\\\\])\r?\n!",($commented?"\\1\\\\\n# ":"\\1\\\\\n"),$value);
+	if($commented)
+		$line = (($oldline && trim($value)==='--COMMENT--')?"#~ $oldline":"# $var = ".str_replace('--COMMENT--','',$value)."\n");
+	elseif($value === '--UNSET--')
+		$line = '';
+	else
+		$line = "$var = ".$value."\n";
+	return $line;
 }
-?>
